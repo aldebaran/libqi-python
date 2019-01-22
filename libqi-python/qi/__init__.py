4fe13681b1dbe9f1aae0a55ea35972905ca59b2d
Jerome Millet <jmillet@softbankrobotics.com>
2019-01-22 12:22:27 +0100

    LibQi does not support Unicode everywhere
    Change-Id: I2674b7486382e6c40d1b4c130c163e44ffd84c8c
    Reviewed-on: http://gerrit.aldebaran.lan/93864
    Reviewed-by: jmillet <jmillet@aldebaran.com>
    Tested-by: jmillet <jmillet@aldebaran.com>

--- libqi-python/qi/__init__.py
+++ libqi-python/qi/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """ QiMessaging Python bindings """
 from __future__ import absolute_import
-from __future__ import unicode_literals
 from __future__ import print_function
 
 import os
