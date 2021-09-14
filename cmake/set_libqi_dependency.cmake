9b3b39f406024f30abfe4929fc207496e70b85ec
philippe.martin <philippe.martin@softbankrobotics.com>
2021-09-14 16:26:32 +0200

    Upgrades libqi to version 2.0.0.
    Change-Id: I6f7640d68da0cf807ea1700e83e354b1974bb38b
    Reviewed-on: http://gerrit2.aldebaran.lan/1524
    Reviewed-by: vincent.palancher <vincent.palancher@softbankrobotics.com>
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Tested-by: philippe.martin <philippe.martin@softbankrobotics.com>

--- cmake/set_libqi_dependency.cmake
+++ cmake/set_libqi_dependency.cmake
@@ -1,4 +1,4 @@
-overridable_variable(LIBQI_VERSION 1.8.7)
+overridable_variable(LIBQI_VERSION 2.0.0)
 
 # Our github clone is sometimes late or is missing tags, so we enable
 # customisation of the URL at configuration time, so users can use another clone.
