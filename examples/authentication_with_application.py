40929b737f46531193e788f22d3e0c723b5b88fb
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2021-09-30 23:03:56 +0200

    examples: Make authentication CLI parsing non-intrusive.
    This ensures `make_application()` can be imported and used by different
    programs, which will also parse their own CLI options.
    
    Change-Id: I0df932210a2c350ce2721153494b252d4f8df89e
    Reviewed-on: http://gerrit2.aldebaran.lan/1562
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Reviewed-by: vincent.palancher <vincent.palancher@softbankrobotics.com>
    Tested-by: vincent.palancher <vincent.palancher@softbankrobotics.com>

--- examples/authentication_with_application.py
+++ examples/authentication_with_application.py
@@ -28,7 +28,7 @@ class AuthenticatorFactory:
 
 
 # Reads a file containing the username on the first line and the password on
-# the second line.
+# the second line. This is the format used by qilaunch.
 def read_auth_file(path):
     with open(path) as f:
         username = f.readline().strip()
@@ -36,27 +36,52 @@ def read_auth_file(path):
         return (username, password)
 
 
-def make_application():
-    app = qi.Application(sys.argv)
-    parser = argparse.ArgumentParser()
+def make_application(argv=sys.argv):
+    """
+    Create and return the qi.Application, with authentication set up
+    according to the command line options.
+    """
+    # create the app and edit `argv` in place to remove the consumed
+    # arguments.
+    # As a side effect, if "-h" is in the list, it is replaced with "--help".
+    app = qi.Application(argv)
+
+    # Setup a non-intrusive parser, behaving like `qi.Application`'s own
+    # parser:
+    # * don't complain about unknown arguments
+    # * consume known arguments
+    # * if the "--help" option is present:
+    #   * print its own options help
+    #   * do not print the main app usage
+    #   * do not call `sys.exit()`
+    parser = argparse.ArgumentParser(add_help=False, usage=argparse.SUPPRESS)
     parser.add_argument(
         "-a", "--authfile",
         help="Path to the authentication config file. This file must "
         "contain the username on the first line and the password on the "
         "second line.")
-    args = parser.parse_args(sys.argv[1:])
+    if "--help" in argv:
+        parser.print_help()
+        return app
+    args, unparsed_args = parser.parse_known_args(argv[1:])
     logins = read_auth_file(args.authfile) if args.authfile else ("nao", "nao")
     factory = AuthenticatorFactory(*logins)
     app.session.setClientAuthenticatorFactory(factory)
+    # edit argv in place.
+    # Note: this might modify sys.argv, like qi.Application does.
+    argv[1:] = unparsed_args
     return app
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--msg", default="Hello python")
     app = make_application()
+    args = parser.parse_args()
     logger = qi.Logger("authentication_with_application")
     logger.info("connecting session")
     app.start()
     logger.info("fetching ALTextToSpeech service")
     tts = app.session.service("ALTextToSpeech")
     logger.info("Saying something")
-    tts.call("say", "Hello python")
+    tts.call("say", args.msg)
